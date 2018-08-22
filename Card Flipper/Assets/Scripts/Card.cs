2cdf170c45c839480a86dc076b17ddd1b92c2cd3
BuildTools <unconfigured@null.spigotmc.org>
2018-08-22 14:48:30 -0500

    placeholder and card highlight
    highlight and click interactions with placeholder added

---
+++ Card Flipper/Assets/Scripts/Card.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class Card : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler, IPointerClickHandler {
+
+    private Image image;
+    private bool hovering = false;
+    private bool selected = false;
+
+    private void Start() {
+        image = GetComponent<Image>();
+    }
+
+    public void OnPointerEnter(PointerEventData eventData) {
+        hovering = true;
+    }
+
+    public void OnPointerExit(PointerEventData eventData) {
+        hovering = false;
+    }
+
+    public void OnPointerClick(PointerEventData eventData) {
+        FlipCard();
+    }
+
+    private void HighLightCard() {
+        if (hovering) {
+            image.color = Color.red;
+        }
+        else {
+            image.color = Color.white;
+        }
+    }
+
+    private void FlipCard() {
+        image.color = Color.blue;
+        selected = true;
+    }
+    
+    private void Update () {
+        if (selected)
+            return;
+
+        HighLightCard();
+	}
+}
---
+++ Card Flipper/Assets/Scripts/CardManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class CardManager : MonoBehaviour
+{
+
+    public int cardAmount;
+
+    GameObject card;
+    List<GameObject> cards = new List<GameObject>();
+
+    void Start()
+    {
+        card = Resources.Load("Card") as GameObject;
+        CreateCards();
+    }
+
+    void CreateCards()
+    {
+        for (int i = 0; i < cardAmount; i++)
+        {
+            cards.Add(Instantiate(card, transform));
+        }
+    }
+
+}
